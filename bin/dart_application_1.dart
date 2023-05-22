40f8bdd93080f90db6854d51275acdc63f1e52b8
Monicka Akilan <monicka.rk@gmail.com>
2023-05-22 17:30:18 +0530

    Added variables and datatypes

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,6 +1,27 @@
 void main() {
   print('Hello world');
   hello();
+  print("~~~~~~~~~~~~~~~~~~~~~~~~");
+  variblesDatatypes();
 }
 
 hello()=> print('from hello function');
+
+void variblesDatatypes(){
+  var a = 5;
+  print(a);
+  print(a.runtimeType);
+
+  String b ='John Doe';
+  print('the value of b is $b');  
+  print('the value of b is ${b.runtimeType}');
+
+  dynamic c;
+  c = 55;
+  print(c);
+  print(c.runtimeType);
+
+  c ='Mark Antony';
+  print('the value of c is $c type: ${c.runtimeType}');  
+
+}
